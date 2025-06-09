33f5e81128a5b98e211711340d14028a1af26625
Angushylesh <sshylesh34@gmail.com>
2025-06-09 00:25:55 -0500

    rechecking the static web page

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,7 +15,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String sayHello() {
-		return "shello";
+		return "podu massuuu";
 	}
 }
 
